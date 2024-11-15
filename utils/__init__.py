7eab8d15826a78e2768d667bdbb97a752072b01b
YangLiwei <3524817551@qq.com>
2024-11-15 22:08:47 +0800

    no thing

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,3 @@
 from utils.config import Config
-<<<<<<< HEAD
 from utils.log import SimpleLogger
-=======
-from utils.log import Logger
->>>>>>> 172f5f07d82b4f358df4ced824ca1d0fa4c8ded7
 from utils.reader import Reader
